ac8936396e9e157911afeb273b45d574a494e138
Jung Rama <jungrama.id@gmail.com>
2023-12-08 17:22:15 +0800

    upgrade package strapi

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -4,16 +4,19 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::product.product', ({ strapi }) => ({
-  async findOne(ctx) {
-    const { id: slug } = ctx.params;
-    const { query } = ctx;
-    if (!query.filters) query.filters = {};
+export default factories.createCoreController(
+	'api::product.product',
+	({ strapi }) => ({
+		async findOne(ctx) {
+			const { id: slug } = ctx.params
+			const { query } = ctx
+			if (!query.filters) query.filters = {}
 
-    query.filters.slug = { $eq: slug };
-    const entity = await strapi.service("api::product.product").find(query);
-    const { results } = await this.sanitizeOutput(entity, ctx);
+			query.filters.slug = { $eq: slug }
+			const entity = await strapi.service('api::product.product').find(query)
+			const { results } = (await this.sanitizeOutput(entity, ctx)) as any
 
-    return this.transformResponse(results[0]);
-},
-}));
+			return this.transformResponse(results[0])
+		},
+	})
+)
